e7027e957975a7fba0dc83519a674dece98133aa
tiagomartins91 <tiago_alex_martins@hotmail.com>
2021-01-29 19:44:22 +0000

    Add database to Application

--- src/main/java/com/example/demo/GraphQlWithSpringBootAppApplication.java
+++ src/main/java/com/example/demo/GraphQlWithSpringBootAppApplication.java
@@ -2,9 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties({LiquibaseProperties.class})
 @ComponentScan
 public class GraphQlWithSpringBootAppApplication {
 
